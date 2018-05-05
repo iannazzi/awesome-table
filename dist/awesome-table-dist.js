ef6de07c698a67c09e15a9a51d73bc1657b14469
Craig Iannazzi <ci@embrasse-moi.com>
2018-05-05 16:48:44 -0400

    module

--- dist/awesome-table-dist.js
+++ dist/awesome-table-dist.js
@@ -1,5 +1,14 @@
-var AwesomeTable =
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["AwesomeTable"] = factory();
+	else
+		root["AwesomeTable"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -414,3 +423,4 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 /***/ })
 
 /******/ });
+});
